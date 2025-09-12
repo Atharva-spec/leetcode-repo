4ac4bae06efa04de2e30e8e2f646cff1dfaf222f
Atharva-spec <84125031+Atharva-spec@users.noreply.github.com>
2025-09-13 01:53:08 +0530

    two-sum.py

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,3 +1,4 @@
+'''basic logic of running one nested for loop iin which it checks all the giving elements in the array and check if the sum of the then is 9 or not in this code written as target'''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
