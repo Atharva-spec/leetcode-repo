bbc35c7a47c2c1fbd62cdb042d2d454671054aaf
Atharva-spec <84125031+Atharva-spec@users.noreply.github.com>
2025-09-14 03:33:48 +0530

    contains-duplicate.py

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,7 +1,7 @@
 #IMP making a hashset in which we will put all the array elements turn by turn and if it encounter aduplicatge it willreturn true 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set() #creating a empty hashset
+        hashset = set() #creating a hashset
         for n in nums:
             if n in hashset: #if a element is already existing inside the set it will return true 
                 return True 
